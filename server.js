f1e4f40edc8cd799cd10739241745d564e0031c1
Prince Kumar <your-email@gmail.com>
2026-05-05 11:41:38 +0530

    add render server config

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+server.use(router)
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`)
+})
